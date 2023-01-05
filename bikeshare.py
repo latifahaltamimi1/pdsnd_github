6ea2be8d0f21a6c6e52b756a5e41d6cce7740c18
Latifah Altamimi <latifahaltmimi1@gmail.com>
2023-01-06 02:31:25 +0300

    refactor: Add to the print in the main method 'see you soon'

--- bikeshare.py
+++ bikeshare.py
@@ -206,7 +206,7 @@ def main():
          
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            print('Thank you ;)')
+            print('Thank you ;) see you soon')
             break
 
 
